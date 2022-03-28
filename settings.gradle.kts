bd73973be512559d5482e3ee5aaada40794a6070
Giao The Cung <giaothe@gmail.com>
2022-03-28 08:51:17 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "dagpenger-SERVICENAME"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven(url = "https://dl.bintray.com/gradle/gradle-plugins")
+    }
+}
