290c9d9f2b7463de0db2b5d061358096b32e317d
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-05-05 16:41:37 +0200

    Legg til style

--- src/main/kotlin/no/nav/dagpenger/soknad/html/HtmlBuilder.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/html/HtmlBuilder.kt
@@ -1,25 +1,45 @@
 package no.nav.dagpenger.soknad.html
 
+import kotlinx.html.HEAD
+import kotlinx.html.STYLE
 import kotlinx.html.body
 import kotlinx.html.div
 import kotlinx.html.dom.createHTMLDocument
 import kotlinx.html.dom.serialize
+import kotlinx.html.h1
 import kotlinx.html.h2
 import kotlinx.html.h3
+import kotlinx.html.head
 import kotlinx.html.html
+import kotlinx.html.lang
+import kotlinx.html.link
 import kotlinx.html.p
+import kotlinx.html.style
+import kotlinx.html.title
+import kotlinx.html.unsafe
 import java.io.File
 
-object HtmlBuilder {
+internal object HtmlBuilder {
     fun lagHtml(htmlModell: HtmlModell): String {
         return createHTMLDocument().html {
+            lang = htmlModell.metaInfo.språk.langAtributt
+            head {
+                title(htmlModell.metaInfo.tittel)
+                fontimports()
+                style {
+                    søknadPdfStyle()
+                }
+            }
             body {
+                h1 {
+                    +htmlModell.metaInfo.hovedOverskrift
+                }
                 htmlModell.seksjoner.forEach { seksjon ->
-                    div {
+                    div(classes = "seksjon") {
                         h2 { +seksjon.overskrift }
                         seksjon.spmSvar.forEach { ss ->
                             h3 { +ss.sporsmal }
-                            p { +"Svar: ${ss.svar}" }
+                            p { +"${htmlModell.metaInfo.språk.svar}: ${ss.svar}" }
                         }
                     }
                 }
@@ -28,27 +48,60 @@ object HtmlBuilder {
     }
 }
 
+private fun STYLE.søknadPdfStyle() {
+    unsafe {
+        //language=CSS
+        raw(
+            """
+                     body{
+                         font-family: 'Source Sans Pro';
+                         padding: 5px;
+                         
+                     }
+                     .seksjon{
+                     }
+
+                            """.trimIndent()
+        )
+    }
+}
+
+private fun HEAD.fontimports() {
+    link {
+        rel = "preconnect"
+        href = "https://fonts.googleapis.com"
+    }
+    link {
+        rel = "preconnect"
+        href = "https://fonts.gstatic.com"
+    }
+    link {
+        href = "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;1,400&display=swap"
+        rel = "stylesheet"
+    }
+}
+
 fun main() {
     HtmlBuilder.lagHtml(
         htmlModell = HtmlModell(
             seksjoner = listOf(
                 HtmlModell.Seksjon(
-                    overskrift = "Seksjon 1",
-                    spmSvar = listOf(
-                        HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 1", svar = "svar 1"),
-                        HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 2", svar = "svar 2"),
-                        HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 3", svar = "svar 3"),
+                    overskrift = "Reel arbeidsøker", spmSvar = listOf(
+                        HtmlModell.SporsmalSvar(sporsmal = "Kan du jobbe både heltid og deltid?", svar = "Ja"),
+                        HtmlModell.SporsmalSvar(sporsmal = "Kan du jobbe i hele Norge?", svar = "Ja"),
+                        HtmlModell.SporsmalSvar(sporsmal = "Kan du ta alle typer arbeid?", svar = "Ja"),
+                        HtmlModell.SporsmalSvar(sporsmal = "Er du villig til å bytte yrke eller gå ned i lønn?", svar = "Ja"),
                     )
 
-                ),
-                HtmlModell.Seksjon(
-                    overskrift = "Seksjon 2",
-                    spmSvar = listOf(
+                ), HtmlModell.Seksjon(
+                    overskrift = "Seksjon 2", spmSvar = listOf(
                         HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 1", svar = "svar 1"),
                         HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 2", svar = "svar 2"),
                         HtmlModell.SporsmalSvar(sporsmal = "Dett er spm 3", svar = "svar 3"),
                     )
                 )
+            ), metaInfo = HtmlModell.MetaInfo(
+                hovedOverskrift = "Søknad om dagpenger"
             )
         )
     ).also {
--- src/main/kotlin/no/nav/dagpenger/soknad/html/Model.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/html/Model.kt
@@ -1,6 +1,13 @@
 package no.nav.dagpenger.soknad.html
 
-data class HtmlModell(val seksjoner: List<Seksjon>) {
+import no.nav.dagpenger.soknad.html.SøknadSpråk.BOKMÅL
+
+internal data class HtmlModell(val seksjoner: List<Seksjon>, val metaInfo:MetaInfo) {
     data class Seksjon(val overskrift: String, val spmSvar: List<SporsmalSvar>)
     data class SporsmalSvar(val sporsmal: String, val svar: String)
+    data class MetaInfo(val hovedOverskrift: String, val tittel: String = hovedOverskrift, val språk: SøknadSpråk= BOKMÅL)
+}
+
+internal enum class SøknadSpråk(val langAtributt:String, val svar: String) {
+    BOKMÅL("no","Svar")
 }
