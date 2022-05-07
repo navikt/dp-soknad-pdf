53c74536900a277ea82ce11b7bfffbf47b04e4f8
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-05-07 20:03:05 +0200

    Hent CSS fra fil
    DAG-224

--- src/main/kotlin/no/nav/dagpenger/soknad/html/HtmlDomUtils.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/html/HtmlDomUtils.kt
@@ -2,14 +2,13 @@ package no.nav.dagpenger.soknad.html
 
 import kotlinx.html.DIV
 import kotlinx.html.HEAD
-import kotlinx.html.STYLE
 import kotlinx.html.div
 import kotlinx.html.h3
-import kotlinx.html.link
 import kotlinx.html.meta
 import kotlinx.html.p
 import kotlinx.html.span
 import kotlinx.html.unsafe
+
 
 internal fun String.xhtmlCompliant() = this
     .replace("&oslash;", "ø")
@@ -30,64 +29,18 @@ internal fun HEAD.pdfa(pdfAKrav: HtmlModell.PdfAKrav) {
     }
 }
 
-internal fun HEAD.fontimports() {
-    link {
-        rel = "preconnect"
-        href = "https://fonts.googleapis.com"
-    }
-    link {
-        rel = "preconnect"
-        href = "https://fonts.gstatic.com"
-    }
-    link {
-        href = "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;1,300&display=swap"
-        rel = "stylesheet"
-    }
-}
-
-internal fun STYLE.søknadPdfStyle() {
+internal fun HEAD.bookmarks(htmlModell: HtmlModell) {
     unsafe {
-        //language=CSS
+        //language=HTML
         raw(
             """
-                     body {
-                         font-family: 'Source Sans Pro';
-                     }
-                     
-                     div{
-                         width: 500px;
-                     }
-                     p {
-                         margin-bottom: 2px;
-                         margin-top: 2px;
-                     }
-                     
-                     h2 {
-                         margin-bottom: 10px;
-                     }
-                     
-                     .seksjon h3 {
-                         margin-bottom: 0;
-                     }
-                     
-                     .boldSpan {
-                         font-weight: bold;
-                     }
-                     
-                     .hjelpetekst {
-                         background-color: rgb(171, 178, 185);
-                         margin-left: 10px;
-                         margin-top: 5px;
-                         padding: 2px;
-                     }
-                     
-                     .infotekst {
-                         margin: 5px 0 0 0;
-                     }
-                     
-                            """.trimIndent()
+                <bookmarks>
+                    <bookmark name="Søknad om dagpenger" href="#hovedoverskrift"></bookmark>
+                </bookmarks>
+            """.trimIndent()
         )
     }
+
 }
 
 internal fun DIV.boldSpanP(boldTekst: String, vanligTekst: String, divider: String = ":") {
@@ -111,3 +64,5 @@ internal fun DIV.spmDiv(spmSvar: HtmlModell.SporsmalSvar, språk: HtmlModell.Sø
         boldSpanP(språk.svar, spmSvar.svar)
     }
 }
+
+internal fun divId(postFix: String) = "div-${postFix.lowercase()}"
