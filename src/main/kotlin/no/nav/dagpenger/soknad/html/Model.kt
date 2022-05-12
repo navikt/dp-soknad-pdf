22dce6e404987ce0d5961ed8c8a2b947ee532ec0
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-05-12 13:10:55 +0200

    Mappe sekjson- og faktatekst
    DAG-224

--- src/main/kotlin/no/nav/dagpenger/soknad/html/Model.kt
+++ src/main/kotlin/no/nav/dagpenger/soknad/html/Model.kt
@@ -6,7 +6,13 @@ internal data class HtmlModell(
     val pdfAKrav: PdfAKrav,
     val infoBlokk: InfoBlokk
 ) {
-    data class Seksjon(val overskrift: String, val spmSvar: List<SporsmalSvar>)
+    data class Seksjon(
+        val overskrift: String,
+        val description: String? = null,
+        val helpText: String? = null,
+        val spmSvar: List<SporsmalSvar>
+    )
+
     data class SporsmalSvar(
         val sporsmal: String,
         val svar: String,
@@ -14,11 +20,13 @@ internal data class HtmlModell(
         val hjelpeTekst: String? = null,
         val oppfølgingspørmål: List<SporsmalSvar>? = null
     )
+
     data class MetaInfo(
         val hovedOverskrift: String,
         val tittel: String = hovedOverskrift,
         val språk: SøknadSpråk = SøknadSpråk.BOKMÅL
     )
+
     data class PdfAKrav(val description: String, val subject: String, val author: String)
     data class InfoBlokk(val fødselsnummer: String, val datoSendt: String)
     enum class SøknadSpråk(
---
+++ src/test/kotlin/no/nav/dagpenger/soknad/serder/SerderTest.kt
@@ -0,0 +1,76 @@
+package no.nav.dagpenger.soknad.serder
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class SerderTest {
+    val faktaJson = object {}.javaClass.getResource("fakta.json")?.readText()
+    val tekstJson = object {}.javaClass.getResource("/tekst.json")?.readText()!!
+
+    @Test
+    fun `Mapper fra json til Seksjoner og SpmOgSvar`() {
+        Oppslag(tekstJson).also {
+            assertEquals("f3", it.lookup("f3").textId)
+            assertEquals("seksjon1", it.lookup("seksjon1").textId)
+        }
+    }
+}
+
+internal class Oppslag(tekstJson: String) {
+    private val objectMapper = jacksonObjectMapper()
+    private val tekstMap = parse(tekstJson)
+    fun lookup(id: String): TekstObjekt = tekstMap[id] ?: throw IllegalArgumentException("Fant ikke tekst til id $id")
+
+    private fun JsonNode.tilFaktaTekstObjekt(): Map<String, TekstObjekt> {
+        val map = mutableMapOf<String, TekstObjekt>()
+        this["SanityTexts"]["fakta"].forEach { tekst ->
+            val textId = tekst["textId"].asText()
+            map[textId] = TekstObjekt.FaktaTekstObjekt(
+                textId = textId,
+                text = tekst["text"].asText(),
+                description = tekst.get("description")?.asText(),
+                helpText = tekst.get("helpText")?.asText(),
+                unit = tekst.get("unit")?.asText()
+            )
+        }
+        return map
+    }
+
+    private fun JsonNode.tilSeksjonTekstObjekt(): Map<String, TekstObjekt> {
+        val map = mutableMapOf<String, TekstObjekt>()
+        this["SanityTexts"]["seksjoner"].forEach { tekst ->
+            val textId = tekst["textId"].asText()
+            map[textId] = TekstObjekt.SeksjonTekstObjekt(
+                textId = textId,
+                title = tekst["title"].asText(),
+                description = tekst.get("description")?.asText(),
+                helpText = tekst.get("helpText")?.asText()
+            )
+        }
+        return map
+    }
+
+    private fun parse(tekstJson: String): Map<String, TekstObjekt> =
+        objectMapper.readTree(tekstJson).let {
+            it.tilFaktaTekstObjekt() + it.tilSeksjonTekstObjekt()
+        }
+
+    sealed class TekstObjekt(val textId: String, val description: String?, val helpText: String?) {
+        class FaktaTekstObjekt(
+            val unit: String? = null,
+            val text: String,
+            textId: String,
+            description: String? = null,
+            helpText: String? = null,
+        ) : TekstObjekt(textId, description, helpText)
+
+        class SeksjonTekstObjekt(
+            val title: String,
+            textId: String,
+            description: String? = null,
+            helpText: String? = null,
+        ) : TekstObjekt(textId, description, helpText)
+    }
+}
