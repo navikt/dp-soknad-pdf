f3823d9d3db3e991a202c494d48cef0492ca41ce
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-07-20 12:38:09 +0200

    sanitydocs
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>

---
+++ docs/sanitystruktur.ts
@@ -0,0 +1,44 @@
+export interface SanityHelpText {
+    title?: string;
+    body: TypedObject | TypedObject[];
+}
+export interface SanityAlertText {
+    title?: string;
+    type: "info" | "warning" | "error" | "success";
+    body: TypedObject | TypedObject[];
+}
+export interface SanityFaktum {
+    textId: string;
+    text: string;
+    description?: TypedObject | TypedObject[];
+    helpText?: SanityHelpText;
+    unit?: string;
+}
+export interface SanitySeksjon {
+    textId: string;
+    title: string;
+    description?: TypedObject | TypedObject[];
+    helpText?: SanityHelpText;
+}
+export interface SanitySvaralternativ {
+    textId: string;
+    text: string;
+    alertText?: SanityAlertText;
+}
+export interface SanityLandGruppe {
+    textId: string;
+    alertText?: SanityAlertText;
+}
+export interface SanityAppTekst {
+    textId: string;
+    valueText: string;
+}
+export interface SanityTexts {
+    fakta: SanityFaktum[];
+    seksjoner: SanitySeksjon[];
+    svaralternativer: SanitySvaralternativ[];
+    landgrupper: SanityLandGruppe[];
+    apptekster: SanityAppTekst[];
+}
+
+export interface TypedObject{}
