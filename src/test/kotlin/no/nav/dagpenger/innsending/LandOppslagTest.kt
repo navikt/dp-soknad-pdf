c7007f12f2b964dd21d1f0b1a1ab9d6cdc7b450b
Giao The Cung <giaothe@gmail.com>
2022-12-16 12:47:28 +0100

    Fikse PDL rare isokoder. Gir f i engelsk enn så lenge.

--- src/test/kotlin/no/nav/dagpenger/innsending/LandOppslagTest.kt
+++ src/test/kotlin/no/nav/dagpenger/innsending/LandOppslagTest.kt
@@ -16,6 +16,15 @@ internal class LandOppslagTest {
         LandOppslag.hentLand(BOKMÅL, "bel").also {
             assertEquals("Belgia", it)
         }
+        LandOppslag.hentLand(BOKMÅL, "xxx").also {
+            assertEquals("Statsløs", it)
+        }
+        LandOppslag.hentLand(BOKMÅL, "xuk").also {
+            assertEquals("Ukjent", it)
+        }
+        LandOppslag.hentLand(BOKMÅL, "xxk").also {
+            assertEquals("Kosovo", it)
+        }
         assertThrows<IllegalArgumentException> {
             LandOppslag.hentLand(BOKMÅL, "nei")
         }
